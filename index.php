2311a2b4c5d02e0c84ab4ecc6db14ecc3ee10ed2
Салям <sam@sessia.dev>
2024-12-23 17:55:37 +0300

    added modifide files

--- index.php
+++ index.php
@@ -5,7 +5,14 @@ $yii=dirname(__FILE__).'/../../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
-// defined('YII_DEBUG') or define('YII_DEBUG',true);
+ defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+function dd($a) {
+    echo "<pre>";
+    var_dump($a);
+    echo "</pre>";
+    die;
+}
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
---
+++ js/popup.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // Обработчик клика на ссылку "Показать"
+    $('.show-popup').click(function (e) {
+        e.preventDefault();
+
+        var popupId = $(this).data('id');
+
+        $.ajax({
+            url: '/popup/incrementViews',
+            type: 'POST',
+            data: {id: popupId},
+            dataType: 'json',
+            success: function (response) {
+                if (response.status === 'success') {
+                    // Отображаем попап
+                    $('#popup-content').html(response.popupContent);
+                    $('#popup-modal').fadeIn();
+                } else {
+                    alert('Ошибка: ' + response.message);
+                }
+            },
+            error: function () {
+                alert('Ошибка при выполнении запроса.');
+            }
+        });
+    });
+
+    // Закрытие попапа
+    $('#close-popup').click(function () {
+        $('#popup-modal').fadeOut();
+    });
+});
